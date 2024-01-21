31b4c6cf4fe5ac05b391e1b492d6ee547ba932e8
SeyedAmirMohammad <41320511+SeyedAmirMohammad@users.noreply.github.com>
2024-01-21 08:37:04 +0330

    nav is scrollable hidden or visiable

--- src/components/complix/Navigation.jsx
+++ src/components/complix/Navigation.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavItem from "../ui/NavItem";
 import Container from "../ui/Container";
 import Logo from "../ui/Logo";
 import "../../styles/complix/navigation.css";
 
 function Navigation({ items, className }) {
+  const [isNavVisible, setNavVisibility] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    setNavVisibility(scrollPosition > currentScrollPos || currentScrollPos < 10);
+    setScrollPosition(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition]);
+
   return (
-    <div className={`nav ${className}`}>
+    <div className={`nav ${className} ${isNavVisible ? "visible" : "hidden"}`}>
       <Container className={"navContainer"}>
         <Logo />
         <NavItem items={items} />
